c287767d6ef94270797ebe9bc7c0a490f810e6f5
박선용 <74956749+small-dogg@users.noreply.github.com>
2025-11-24 19:30:26 +0900

    [level 3] Title: 여행경로, Time: 30.86 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,47 +2,39 @@ import java.util.*;
 
 public class Solution {
     public String[] solution(String[][] tickets) {
-        String[] answer = {};
-        //ticket은 {출발지,목적지} 배열
-        //먼저 인접리스트를 만들고, 정렬 (하려했으나, 필요없는듯)
+        Arrays.sort(tickets, ((a, b) -> {
+            int i = a[0].compareTo(b[0]);
+            if (i == 0) return a[1].compareTo(b[1]);
+            else return i;
+        }));
 
-        //출발지, 도착지 기준으로 정렬
-        Arrays.sort(tickets, (a, b) -> {
-            if (!a[0].equals(b[0])) return a[0].compareTo(b[0]);
-            return a[1].compareTo(b[1]);
-        });
-
-        //티켓 수 만큼 방문 배열 선얼
         boolean[] visited = new boolean[tickets.length];
-        List<String> result = new ArrayList<>();
 
-        List<String> now = new ArrayList<>();
-        now.add("ICN");
-        dfs("ICN", visited, tickets, 0, now, result);
+        List<String> nowList = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        nowList.add("ICN");
+        dfs("ICN", tickets, visited, nowList, result);
 
         return result.toArray(String[]::new);
     }
 
-    //현재 위치, 방문배열, 티켓, 방문횟수, 결과
-    private void dfs(String now, boolean[] visited, String[][] tickets, int usedCount, List<String> nowList, List<String> result) {
-        //종료조건 : 더이상 나아갈 곳이 없거나, 사용된 티켓이 티켓수 만큼 사용되었을때
-        if (usedCount == tickets.length) {
+    private void dfs(String now, String[][] ticket, boolean[] visited, List<String> nowList, List<String> result) {
+        if (nowList.size() == ticket.length + 1){
             if(result.isEmpty()) result.addAll(nowList);
             return;
         }
 
-        for (int i = 0; i < tickets.length; i++) {
-            //방문하지 않은 곳이면
-            if (!visited[i] && tickets[i][0].equals(now)) {
-                String next = tickets[i][1];
+        for (int i = 0; i < ticket.length; i++) {
+            String src = ticket[i][0];
+            String dest = ticket[i][1];
 
+            if (src.equals(now) && !visited[i]) {
                 visited[i] = true;
-                nowList.add(next);
-                dfs(next, visited, tickets, usedCount + 1, nowList, result);
+                nowList.add(dest);
+                dfs(dest, ticket, visited, nowList, result);
                 nowList.remove(nowList.size() - 1);
                 visited[i] = false;
             }
         }
-
     }
 }
