bcdee51e0d67f6ff75b3965a28e6364bbaa78635
박선용 <74956749+small-dogg@users.noreply.github.com>
2025-11-24 18:23:58 +0900

    [level 3] Title: 단어 변환, Time: 0.46 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,50 +1,51 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class Solution {
-    class Node {
-        String word;
+public class Solution {
+    class State {
+        String val;
         int count;
 
-        public Node(String word, int count) {
-            this.word = word;
+        public State(String val, int count) {
+            this.val = val;
             this.count = count;
         }
     }
 
     public int solution(String begin, String target, String[] words) {
-        //한번에 한개의 알파벳만 변경 가능
-        //words에 있는 단어로만 변경 가능
+        Queue<State> q = new LinkedList<>();
+        q.offer(new State(begin, 0));
 
-        Queue<Node> q = new LinkedList<>();
         boolean[] visited = new boolean[words.length];
-
-        q.offer(new Node(begin, 0));
         while (!q.isEmpty()) {
-            Node now = q.poll();
+            State now = q.poll();
+            String val = now.val;
+            int count = now.count;
 
-            if (now.word.equals(target)) {
-                return now.count;
+            if (val.equals(target)) {
+                return count;
             }
-            
+
             for (int i = 0; i < words.length; i++) {
                 String word = words[i];
-                if (isDiffOnlyOne(word, now.word)) {
+                if (diffOnlyOne(val, word)) {
                     if (!visited[i]) {
                         visited[i] = true;
-                        q.offer(new Node(word, now.count+1));
+                        q.offer(new State(word, count + 1));
                     }
                 }
             }
         }
+
         return 0;
     }
-    
-    private boolean isDiffOnlyOne(String a, String b) {
-    int count = 0;
-    for (int i = 0; i < a.length(); i++) {
-        if (a.charAt(i) != b.charAt(i)) count++;
-        if (count > 1) return false;
-    }
-    return count == 1;
+    private boolean diffOnlyOne(String val, String word) {
+        int count = 0;
+        for (int i = 0; i < val.length(); i++) {
+            if (val.charAt(i) != word.charAt(i)) count++;
+            if(count >1) return false;
+        }
+
+        return count == 1;
     }
 }
