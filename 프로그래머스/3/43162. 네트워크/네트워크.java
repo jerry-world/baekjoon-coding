ddf5f2ba135c5e61917cdff6a5c58c16ff2a69d6
박선용 <74956749+small-dogg@users.noreply.github.com>
2025-11-27 22:02:13 +0900

    [level 3] Title: 네트워크, Time: 0.71 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,39 +1,41 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public int solution(int n, int[][] computers) {
-        //n 컴퓨터 수
-        //computers : 양방향 인접리스트
-
-        //bfs로 풀이(전체 순회해야함)
-
-        //방문배열
-        boolean[] visited = new boolean[n];
-
-        int network = 0;
-        Queue<Integer> q = new LinkedList<>();
-        //각 컴퓨터마다 순회하며 bfs 시작점 획득
+        List<Integer>[] graph = new ArrayList[n];
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                network++;
-                q.offer(i);
-                bfs(i, n, visited, computers);
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < computers.length; i++) {
+            for (int j = 0; j < computers[i].length; j++) {
+                if (computers[i][j] == 1 && i != j) {
+                    graph[i].add(j);
+                }
             }
         }
-        return network;
+
+
+        int cnt = 0;
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, graph, visited);
+                cnt++;
+            }
+        }
+
+        return cnt;
     }
 
-    private void bfs(int start, int n, boolean[] visited, int[][] computers) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
+    private void dfs(int now, List<Integer>[] graph, boolean[] visited) {
+        if (visited[now]) return;
 
-        while (!q.isEmpty()) {
-            int now = q.poll();
-            for (int i = 0; i < n; i++) {
-                if (!visited[i] && computers[now][i] == 1) {
-                    visited[i] = true;
-                    q.offer(i);
-                }
+        visited[now] = true;
+
+        for (int next : graph[now]) {
+            if (!visited[next]) {
+                dfs(next, graph, visited);
             }
         }
     }
