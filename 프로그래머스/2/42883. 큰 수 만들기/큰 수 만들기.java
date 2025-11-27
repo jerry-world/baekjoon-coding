2043344d6b84b23c5388612dbfc3b3abcb082ace
박선용 <74956749+small-dogg@users.noreply.github.com>
2025-11-28 01:34:29 +0900

    [level 2] Title: 큰 수 만들기, Time: 43.04 ms, Memory: 98.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : number.toCharArray()) {
+            while (sb.length() > 0 && sb.charAt(sb.length() - 1) < c && k > 0) {
+                sb.deleteCharAt(sb.length() - 1);
+                k--;
+            }
+
+            sb.append(c);
+        }
+
+        while (k > 0) {
+            sb.deleteCharAt(sb.length() - 1);
+            k--;
+        }
+
+        return sb.toString();
+    }
+}
